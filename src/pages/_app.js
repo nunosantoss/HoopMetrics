f64024d4c590edad6ce24252e2185e63194854fd
nunosantoss <nunongan.dev@gmail.com>
2024-04-19 21:03:03 +0100

    Added some requests

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
 import "@/styles/globals.css";
 
-
 export const metadata = {
-  title: "Nuno Santos Portfolio",
-  description: "PWA Portfolio",
+  title: "HoopMetrics",
+  description: "NBA Statistics App",
   generator: "Next.js",
-  manifest: "/manifest.json",
-  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#262626" }],
+  manifest: "manifest.json",
+  keywords: ["nba", "statistics", "hoopmetrics", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#dc2626" }],
   authors: [
     { name: "Nuno Santos" },
     {
@@ -18,11 +17,15 @@ export const metadata = {
   viewport:
     "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
-    { rel: "apple-touch-icon", url: "icons/128.png" },
-    { rel: "icon", url: "icons/128.png" },
+    { rel: "apple-touch-icon", url: "ios/128.png" },
+    { rel: "icon", url: "ios/128.png" },
   ],
 };
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,65 +4,88 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Main */}
+        <meta charSet="utf-8" />
+        <meta name="description" content="HoopMetrics" />
+        <meta
+          name="keywords"
+          content="nba, statistics, hoop, nunosantos, metrics"
+        />
 
-{/* Main */}
-<meta charSet="utf-8"/>
-<meta name="description" content="Nuno Santos Portfolio" />
-<meta name="keywords" content="portfolio, pwa, nextjs, nunosantos, cv" />
+        {/* Android */}
+        <meta name="theme-color" content="#dc2626" />
+        <meta name="mobile-web-app-capable" content="yes" />
 
-{/* Android */}
-<meta name="theme-color" content="#171717"/>
-<meta name="mobile-web-app-capable" content="yes"/>
+        {/* iOS */}
+        <meta name="apple-mobile-web-app-title" content="HoopMetrics" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
-{/* iOS */}
-<meta name="apple-mobile-web-app-title" content="Nuno Santos "/>
-<meta name="apple-mobile-web-app-capable" content="yes"/>
-<meta name="apple-mobile-web-app-status-bar-style" content="default"/>
+        {/* Windows */}
+        <meta name="msapplication-navbutton-color" content="#dc2626" />
+        <meta name="msapplication-TileColor" content="#dc2626" />
+        {/* <meta name="msapplication-TileImage" content="ms-icon-144x144.png" /> */}
+        <meta name="msapplication-config" content="browserconfig.xml" />
 
-{/* Windows */}
-<meta name="msapplication-navbutton-color" content="red"/>
-<meta name="msapplication-TileColor" content="#171717"/>
-<meta name="msapplication-TileImage" content="ms-icon-144x144.png"/>
-<meta name="msapplication-config" content="browserconfig.xml"/>
+        {/* Pinned Sites */}
+        <meta name="application-name" content="HoopMetrics" />
+        <meta name="msapplication-tooltip" content="NBA Statistics App" />
+        <meta name="msapplication-starturl" content="/" />
 
-{/* Pinned Sites */}
-<meta name="application-name" content="Nuno Santos"/>
-<meta name="msapplication-tooltip" content="Tooltip Text"/>
-<meta name="msapplication-starturl" content="/"/>
+        {/* Pinned Sites */}
+        <meta name="msapplication-tap-highlight" content="no" />
 
-{/* Pinned Sites */}
-<meta name="msapplication-tap-highlight" content="no"/>
+        {/* UC Mobile Browser */}
+        <meta name="full-screen" content="yes" />
+        <meta name="browsermode" content="application" />
 
-{/* UC Mobile Browser */}
-<meta name="full-screen" content="yes"/>
-<meta name="browsermode" content="application"/>
+        {/* Orientation */}
+        <meta name="screen-orientation" content="portrait" />
 
-{/* Orientation */}
-<meta name="screen-orientation" content="portrait"/>
+        {/* Main Link Tags  */}
+        <link
+          href="favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          href="android/android-launchericon-48-48.png"
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+        />
 
+        {/* Android  */}
+        <link
+          href="android/android-chrome-192x192.png"
+          rel="icon"
+          sizes="192x192"
+        />
+        <link href="ios/128.png" rel="icon" sizes="128x128" />
 
-{/* Main Link Tags  */}
-<link href="favicon-16x16.png" rel="icon" type="image/png" sizes="16x16"/>
-<link href="favicon-32x32.png" rel="icon" type="image/png" sizes="32x32"/>
-<link href="android/android-launchericon-48-48.png" rel="icon" type="image/png" sizes="48x48"/>
+        {/* UC Browser  */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="ios/apple-touch-icon.png"
+        />
+        <link
+          rel="mask-icon"
+          href="ios/safari-pinned-tab.svg"
+          color="#dc2626"
+        />
 
-{/* Android  */}
-<link href="android-chrome-192x192.png" rel="icon" sizes="192x192"/>
-<link href="android/128.png" rel="icon" sizes="128x128"/>
-
-
-{/* UC Browser  */}
-<link rel="apple-touch-icon" sizes="ios/180x180" href="apple-touch-icon.png"/>
-<link rel="mask-icon" href="safari-pinned-tab.svg" color="#171717"/>
-
-
-{/* Manifest.json  */}
-<link rel="manifest" href="manifest.json" />
-
-
-
-</Head>
-      <body>
+        {/* Manifest.json  */}
+        <link rel="manifest" href="manifest.json" />
+      </Head>
+      <body className="bg-white dark:bg-neutral-800">
         <Main />
         <NextScript />
       </body>
