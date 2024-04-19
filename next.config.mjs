18da76235ad9a0f0ab05557190d48066e7f0dbac
nsantos <nsantos@egapi.pt>
2024-04-19 17:01:17 +0100

    Added Assets + Manifest + Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
+/** @type {import('next-pwa').PWAConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
 };
 
-export default nextConfig;
+const pwaConfig = {
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+};
+
+export default { ...nextConfig, ...pwaConfig };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
 import "@/styles/globals.css";
 
+
+export const metadata = {
+  title: "Nuno Santos Portfolio",
+  description: "PWA Portfolio",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#262626" }],
+  authors: [
+    { name: "Nuno Santos" },
+    {
+      name: "Nuno Santos",
+      url: "https://www.linkedin.com/in/nunosantoss/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/128.png" },
+    { rel: "icon", url: "icons/128.png" },
+  ],
+};
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
